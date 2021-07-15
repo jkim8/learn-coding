1b0472a0f1bf15d468e3b8a28b3cf2accfb8cea3
naujkim <juno0218@naver.com>
2021-07-15 17:17:31 -0400

    learn useState

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import React, { useState } from 'react'
+import logo from './logo.svg'
 import './App.css';
 
-function App() {
+function App(){
+ 
+  let posts = '강남 고기 맛집';
+  const [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동 맛집'])
+  //state가 바뀌면 HTML이 재렌더링 된다. 새로고침 없이
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <div>개발 blog</div>
+      </div>
+      <div className="list">
+        <h3>{ 글제목[1] }</h3>
+        <p>2월 17일 발행</p>
+        <hr/>
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
