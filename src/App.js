caf29e976f57fe24c60823cda3926230a4c760fa
naujkim <juno0218@naver.com>
2021-07-16 08:57:38 -0400

    use component

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App(){
       <div className="black-nav">
         <div>개발 blog</div>
       </div>
-      <button onClick={제목바꾸기}>버튼</button>
+      {/* <button onClick={제목바꾸기}>버튼</button> */}
       <div className="list">
         <h3>{ 글제목[0] } <span onClick={()=> { 따봉변경(따봉+1) }} >👍</span> {따봉} </h3>
         <p>2월 17일 발행</p>
@@ -38,8 +38,22 @@ function App(){
         <h3>{ 글제목[2] }</h3>
         <p>2월 17일 발행</p>
         <hr/>
-      </div>    
+      </div> 
+       
+      <Modal />
+
     </div>
+  )
+}
+
+
+function Modal() {
+  return (
+    <div className="modal">
+      <h2>제목</h2>
+      <p>날짜</p>
+      <p>상세내용</p>
+    </div> 
   )
 }
 
