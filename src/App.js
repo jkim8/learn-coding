858d2fa42d8a6dbcdcfe04edd28561a2c95d41ec
naujkim <juno0218@naver.com>
2021-07-16 08:37:48 -0400

    state change using spread operator

--- src/App.js
+++ src/App.js
@@ -1,22 +1,44 @@
+/*eslint-disable*/
+
 import React, { useState } from 'react'
 import logo from './logo.svg'
 import './App.css';
 
 function App(){
  
-  let posts = '강남 고기 맛집';
-  const [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동 맛집'])
+  let [글제목, 글제목변경] = useState( ['남자코트 추천', '강남 우동맛집', '파이썬 독학'] );
+  let [따봉, 따봉변경] =  useState(0)
   //state가 바뀌면 HTML이 재렌더링 된다. 새로고침 없이
+
+
+  function 제목바꾸기() {
+    var newArray = [...글제목]
+    newArray[0] = '여자코트추천'
+    글제목변경(newArray)
+  }
+
+
   return (
     <div className="App">
       <div className="black-nav">
         <div>개발 blog</div>
+      </div>
+      <button onClick={제목바꾸기}>버튼</button>
+      <div className="list">
+        <h3>{ 글제목[0] } <span onClick={()=> { 따봉변경(따봉+1) }} >👍</span> {따봉} </h3>
+        <p>2월 17일 발행</p>
+        <hr/>
       </div>
       <div className="list">
         <h3>{ 글제목[1] }</h3>
         <p>2월 17일 발행</p>
         <hr/>
       </div>
+      <div className="list">
+        <h3>{ 글제목[2] }</h3>
+        <p>2월 17일 발행</p>
+        <hr/>
+      </div>    
     </div>
   )
 }
